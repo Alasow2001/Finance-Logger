1b40de801c2592f3319c4a0beb19faf88446da04
Ahmed Alasow <a.alasow01@gmail.com>
2023-09-12 15:36:32 +0100

    Initialised the payment class

--- public/classes/Payment.js
+++ public/classes/Payment.js
@@ -1,2 +1,10 @@
 export class Payment {
+    constructor(recepient, amount, details) {
+        this.recepient = recepient;
+        this.amount = amount;
+        this.details = details;
+    }
+    format() {
+        return `${this.recepient} is owed ${this.amount} for ${this.details}`;
+    }
 }
--- src/classes/Payment.ts
+++ src/classes/Payment.ts
@@ -1,3 +1,14 @@
-export class Payment{
-    
+import { HasFormatter } from "../interfaces/HasFormatter"
+
+export class Payment implements HasFormatter{
+
+    constructor(
+        readonly recepient: string,
+        public amount: number,
+        private details: string,
+    ) {}
+
+    format() {
+        return `${this.recepient} is owed ${this.amount} for ${this.details}`
+    }
 }
