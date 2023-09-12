905093a3aa3aba6c80a1bb68168672f2474e6e0f
Ahmed Alasow <a.alasow01@gmail.com>
2023-09-12 18:31:48 +0100

    Created an if statement to check the type of financial document

--- public/script.js
+++ public/script.js
@@ -1,10 +1,19 @@
 import { Invoice } from './classes/Invoice.js';
-const firstInvoice = new Invoice('Steven', 250, 'work on website project');
-const secondInvoice = new Invoice('Josh', 220, 'testing the website project');
-let invoices = [];
-invoices.push(firstInvoice);
-invoices.push(secondInvoice);
-console.log(invoices);
+import { Payment } from './classes/Payment.js';
+let docOne;
+let docTwo;
+docOne = new Invoice("Brian", 280, "Work on the database");
+docTwo = new Payment("Liam", 300, "for his weekly wage");
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
+// const firstInvoice = new Invoice('Steven', 250, 'work on website project')
+// const secondInvoice = new Invoice('Josh', 220, 'testing the website project')
+// let invoices : Invoice[] = []
+// invoices.push(firstInvoice)
+// invoices.push(secondInvoice)
+// console.log(invoices)
 const form = document.querySelector('form');
 // console.log(form.children)
 const type = document.querySelector('#type');
@@ -14,5 +23,13 @@ const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     // Prevents the page from refreshing after the button is pressed
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    // Checking whether the type of finance logged is an invoice or a payment, printing the result into the console
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, amount.valueAsNumber, details.value);
+    }
+    else {
+        doc = new Payment(toFrom.value, amount.valueAsNumber, details.value);
+    }
+    console.log(doc);
 });
--- src/script.ts
+++ src/script.ts
@@ -2,15 +2,26 @@ import {Invoice} from './classes/Invoice.js'
 import {Payment} from './classes/Payment.js'
 import { HasFormatter } from "./interfaces/HasFormatter.js"
 
+let docOne : HasFormatter
+let docTwo: HasFormatter
 
-const firstInvoice = new Invoice('Steven', 250, 'work on website project')
-const secondInvoice = new Invoice('Josh', 220, 'testing the website project')
+docOne = new Invoice("Brian", 280, "Work on the database")
+docTwo = new Payment("Liam", 300, "for his weekly wage")
 
-let invoices : Invoice[] = []
-invoices.push(firstInvoice)
-invoices.push(secondInvoice)
+let docs : HasFormatter[] = []
+docs.push(docOne)
+docs.push(docTwo)
 
-console.log(invoices)
+console.log(docs)
+
+// const firstInvoice = new Invoice('Steven', 250, 'work on website project')
+// const secondInvoice = new Invoice('Josh', 220, 'testing the website project')
+
+// let invoices : Invoice[] = []
+// invoices.push(firstInvoice)
+// invoices.push(secondInvoice)
+
+// console.log(invoices)
 
 const form = document.querySelector('form') as HTMLFormElement;
 // console.log(form.children)
@@ -24,11 +35,15 @@ form.addEventListener('submit', (e: Event) => {
     // Prevents the page from refreshing after the button is pressed
     e.preventDefault();
 
-    console.log(
-        type.value, 
-        toFrom.value, 
-        details.value, 
-        amount.valueAsNumber
-    )
+    let doc: HasFormatter
+
+    // Checking whether the type of finance logged is an invoice or a payment, printing the result into the console
+    if (type.value === 'invoice'){
+        doc = new Invoice(toFrom.value, amount.valueAsNumber, details.value)
+    } else {
+        doc = new Payment(toFrom.value, amount.valueAsNumber, details.value)
+    }
+
+    console.log(doc);
 })
 
