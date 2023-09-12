e376f61eec8240301bcc57ae5f29023d7f3c527a
Ahmed Alasow <a.alasow01@gmail.com>
2023-09-12 14:32:21 +0100

    Added an event listener to stop the page from refreshing and type casting to each of the inputs

--- public/script.js
+++ public/script.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello world");
+const form = document.querySelector('form');
+// console.log(form.children)
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#toFrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    // Prevents the page from refreshing after the button is pressed
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
